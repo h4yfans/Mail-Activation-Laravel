1dc72dbeb921ecd1d62b2b2b2a8910bb291c0ac4
Kaan Karaca <kaan94karaca@gmail.com>
2016-11-16 01:42:47 +0300

    activating user and autologin

--- app/Http/Controllers/Auth/ActivationController.php
+++ app/Http/Controllers/Auth/ActivationController.php
@@ -5,12 +5,21 @@ namespace App\Http\Controllers\Auth;
 use App\ActivationToken;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ActivationController extends Controller
 {
     public function activate(ActivationToken $token)
     {
-        dd($token);
+        $token->user()->update([
+            'active' => true
+        ]);
+
+        $token->delete();
+
+        Auth::login($token->user, true);
+
+        return redirect('/home');
     }
 
     public function resend(Request $request)
